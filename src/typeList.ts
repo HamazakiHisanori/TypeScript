bb7efda0194bca3c7138bd2eba9098f071f1ca24
HamazakiHisanori <81221219+HamazakiHisanori@users.noreply.github.com>
2021-08-31 16:16:08 +0900

    Type list how to use

---
+++ src/typeList.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/** TypeScriptの基本の方 */
+
+// boolean
+const bool: boolean = true;
+
+// number
+const num: number = 0;
+
+// string
+const str: string = "str";
+
+// Array
+const arr1: Array<number> = [0, 1, 2];
+const arr2: number[] = [0, 1, 2];
+
+// tuple
+const tuple: [number, string] = [0, "str"];
+
+// any
+const any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "sample";
+};
+
+// null
+const null1: null = null;
+
+// undefined
+const undefined1: undefined = undefined;
+
+// object
+const ob1j1: object = {};
+const obj2: {} = {};
